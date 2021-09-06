e4ae5948f0077146aa40cdc995641ec8e091af7c
fallkhady <87422360+fallkhady@users.noreply.github.com>
2021-09-06 17:06:56 +0200

    Add files via upload

---
+++ space invaders/test.js
@@ -0,0 +1,18 @@
+// function Sprite (filename,left,top){
+//     this._node = document.createElement("img");
+//     this._node.src = filename;
+//     this._node.style.position = "absolute";
+//     document.body.appendChild[this._node];
+
+
+//     Object.defineProperty( this, "left",{
+//         get: function() {
+//             return this._left;
+//         },
+//         set: function( value) {
+//             this._left = value;
+//             this._node.style.left = value + "px";
+//         }
+//     }); 
+
+// }
